d14819ab56f7987bae4171680eb317e5b0827fd0
Filip Pobořil <tsuki@fpob.cz>
2023-05-16 18:41:59 +0200

    Fix PyPy build script

--- scripts/build-pypy.sh
+++ scripts/build-pypy.sh
@@ -3,23 +3,23 @@ set -o errexit -o nounset -o pipefail
 
 PYTHON_VERSION=$1
 PYPY_VERSION=$2
-PYPY_VERSION_FULL=$(
+ARCHIVE_NAME=$(
     curl -s --compressed https://downloads.python.org/pypy/ \
-        | grep -Po "pypy${PYTHON_VERSION//./\\.}-v${PYPY_VERSION//./\\.}\.\d+" \
+        | grep -Po "pypy${PYTHON_VERSION//./\\.}-v${PYPY_VERSION//./\\.}\.\d+-linux64" \
         | sort -uV | tail -n1
 )
 
-if [[ -z "${PYPY_VERSION_FULL}" ]] ; then
+if [[ -z "${ARCHIVE_NAME}" ]] ; then
     echo "Failed to find latest PyPy ${PYTHON_VERSION} ${PYPY_VERSION} version!" >&2
     exit 1
 fi
 
 cd /tmp
 
-wget -q https://downloads.python.org/pypy/${PYPY_VERSION_FULL}-linux64.tar.bz2
-tar -xjf ${PYPY_VERSION_FULL}-linux64.tar.bz2
+wget -q https://downloads.python.org/pypy/${ARCHIVE_NAME}.tar.bz2
+tar -xjf ${ARCHIVE_NAME}.tar.bz2
 
-mv ${PYPY_VERSION_FULL}-linux64 /opt/pypy${PYTHON_VERSION}
+mv ${ARCHIVE_NAME} /opt/pypy${PYTHON_VERSION}
 
 # Starting with PyPy 7.3.5, tarball contains python* binaries - remove them to
 # avoid conflicts with CPython.
